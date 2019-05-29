524ad37bd9fc6b7f1ddb758f577c97a1e8dc5616
Dale Watson <dellryuzi@gmail.com>
2019-05-29 22:23:19 +0700

    Add limitation

--- Quiz.js
+++ Quiz.js
@@ -9,54 +9,54 @@ const solution = relation => {
 
     if(relation.length > 20 || relation[0].length > 8){
         return "Table format is wrong, limit are 20 rows and 8 columns"
-    }else {
-        /**
-         * finding if column is already unique and identical key
-         * or super key ( the key with absolute unique for each relation and usually is an id ) 
-         * 
-         */
-        for(let i = 0; i < relation[0].length; i++ ){ arr.push([]) }
-        for(let i = 0; i < relation.length; i++ ){
-            
-            for(let j = 0; j < relation[i].length; j++ ){
-                arr[j].push(relation[i][j])
-            }
-        }
+    }
     
-        //checking if single column has a duplication
-        arr.map(item => {
-            let duplicate = false
+    /**
+     * finding if column is already unique and identical key
+     * or super key ( the key with absolute unique for each relation and usually is an id ) 
+     * 
+     */
+    for(let i = 0; i < relation[0].length; i++ ){ arr.push([]) }
+    for(let i = 0; i < relation.length; i++ ){
+        
+        for(let j = 0; j < relation[i].length; j++ ){
+            arr[j].push(relation[i][j])
+        }
+    }
 
-            item.filter((value, index) => {
-                if(item.indexOf(value) != index){
+    //checking if single column has a duplication
+    arr.map(item => {
+        let duplicate = false
+
+        item.filter((value, index) => {
+            if(item.indexOf(value) != index){
+                duplicate = true
+            }
+        })
+
+        duplicate ? saveNonKey.push(arr.indexOf(item)) : answer += 1
+    })
+
+    //the duplication column would be re-check to satisy the minimal condition 
+    for(let i = 0; i < saveNonKey.length -1; i++) {
+        for(let j = i +1; j < saveNonKey.length; j++){
+            let combine = []
+            let duplicate = false
+            for(let k = 0; k < arr[saveNonKey[i]].length; k++) {
+                combine.push(arr[saveNonKey[i]][k] + arr[saveNonKey[j]][k])
+            }
+
+            //this can be created as a method, to avoid DRY
+            combine.filter((value, index) => {
+                if(combine.indexOf(value) != index){
                     duplicate = true
                 }
             })
-    
-            duplicate ? saveNonKey.push(arr.indexOf(item)) : answer += 1
-        })
-    
-        //the duplication column would be re-check to satisy the minimal condition 
-        for(let i = 0; i < saveNonKey.length -1; i++) {
-            for(let j = i +1; j < saveNonKey.length; j++){
-                let combine = []
-                let duplicate = false
-                for(let k = 0; k < arr[saveNonKey[i]].length; k++) {
-                    combine.push(arr[saveNonKey[i]][k] + arr[saveNonKey[j]][k])
-                }
-    
-                //this can be created as a method, to avoid DRY
-                combine.filter((value, index) => {
-                    if(combine.indexOf(value) != index){
-                        duplicate = true
-                    }
-                })
-                duplicate ? null : answer +=1
-            }
+            duplicate ? null : answer +=1
         }
-    
-        return answer;
     }
+
+    return answer;
 
 }
 
