8eef38f344e157904c4966cc34b84f170bf9006d
Dale Watson <dellryuzi@gmail.com>
2019-05-29 20:22:31 +0700

    Update Quiz 3 success answer

--- Quiz.js
+++ Quiz.js
@@ -2,7 +2,7 @@ const solution = relation => {
     let answer = 0;
     let arr = []
     let saveNonKey = []
-    let compare = []
+
     //preventing the super key to be involved, find which column has the real super key
     for(let i = 0; i < relation[0].length; i++ ){ arr.push([]) }
     for(let i = 0; i < relation.length; i++ ){
@@ -12,7 +12,7 @@ const solution = relation => {
         }
     }
 
-    //checking if single row has non duplicate = + key -- 
+    //checking if single row has non duplicate 
     arr.map(item => {
         let duplicate = false
 
@@ -22,27 +22,30 @@ const solution = relation => {
             }
         })
 
-        if(duplicate){
-            saveNonKey.push(arr.indexOf(item))
-        }else{
-            answer += 1
-        }
+        duplicate ? saveNonKey.push(arr.indexOf(item)) : answer += 1
     })
 
-    console.log(arr)
-    console.log(saveNonKey)
+    //checking for the minimal condition
     for(let i = 0; i < saveNonKey.length -1; i++) {
-        
         for(let j = i +1; j < saveNonKey.length; j++){
+            let combine = []
+            let duplicate = false
             for(let k = 0; k < arr[saveNonKey[i]].length; k++) {
-                compare.push(arr[saveNonKey[i]][k] + arr[saveNonKey[j]][k])
+                combine.push(arr[saveNonKey[i]][k] + arr[saveNonKey[j]][k])
             }
+
+            //this should be a method, to avoid DRY
+            combine.filter((value, index) => {
+                if(combine.indexOf(value) != index){
+                    duplicate = true
+                }
+            })
+
+            duplicate ? null : answer +=1
         }
     }
 
-
-
-    return compare;
+    return answer;
 }
 
 
